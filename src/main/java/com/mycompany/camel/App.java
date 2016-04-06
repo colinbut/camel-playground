b4bec39672f56a5c485044e91a704c35aa7f8ccf
Colin But <colinbut@users.noreply.github.com>
2016-04-06 20:51:27 +0100

    first stab at a camel

--- src/main/java/com/mycompany/camel/App.java
+++ src/main/java/com/mycompany/camel/App.java
@@ -1,13 +1,38 @@
 package com.mycompany.camel;
 
+import org.apache.camel.CamelContext;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+    /**
+     * Main method
+     *
+     * @param args
+     * @throws Exception
+     */
+    public static void main( String[] args ) throws Exception {
+
+        CamelContext camelContext = new DefaultCamelContext();
+
+        camelContext.addRoutes(new RouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+                from("file:data/inbox")
+                        .to("file:data/outbox");
+            }
+        });
+
+        camelContext.start();
+
+        Thread.sleep(3000);
+
+        camelContext.stop();
+
     }
 }
